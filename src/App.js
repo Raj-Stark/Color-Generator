12a36f0394a68cc5db4c58a497fc9a5e9f4aa2e7
Raj-Stark <rpal778866@gmail.com>
2022-06-16 16:11:27 +0530

    Final Push

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#f15025").all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
+      setList(colors);
       console.log(colors);
+      setError(false);
     } catch (e) {
       setError(true);
       console.log(e);
@@ -37,9 +39,21 @@ function App() {
           </button>
         </form>
       </section>
-      ;
       <section className="colors">
-        <h4>List Goes here </h4>
+        {list.map((color, index) => {
+          console.log(color);
+
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            >
+              {" "}
+            </SingleColor>
+          );
+        })}
       </section>
     </>
   );
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
 
-export default SingleColor
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setAlert(false);
+    }, 1000);
+
+    return () => clearTimeout(timeOut);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 8 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(`#${hexColor}`);
+      }}
+    >
+      <p className="percent-value">{weight} %</p>
+      <p className="color-value">{`#${hexColor}`}</p>
+      {alert && <p className="alert"> Copied To Clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
